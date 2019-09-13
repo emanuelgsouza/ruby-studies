445b9c0e9fee942eef22914fb2394e0dc05cfba8
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-13 13:52:44 -0300

    create categorize function

--- imc.rb
+++ imc.rb
@@ -3,6 +3,28 @@ def get_user_input(msg)
     return gets
 end
 
+# used https://pt.wikipedia.org/wiki/Índice_de_massa_corporal
+def categorize(value)
+    if (value < 17)
+        return "Muito abaixo do peso"
+    end
+
+    case value
+    when 17.1...18.49
+        return "Abaixo do peso"
+    when 18.5...24.99
+        return "Peso normal"
+    when 25...29.99
+        return "Acima do peso"
+    when 30...34.99
+        return "Obesidade I"
+    when 35...39.99
+        return "Obesidade II (severa)"
+    else
+        return "Acima de 40	Obesidade III (mórbida)"
+    end
+end
+
 puts "Bem vindo a aplicação de IMC"
 
 nome = get_user_input("Informe seu nome: ")
@@ -17,3 +39,4 @@ puts "Paciente #{nome}"
 puts "Altura #{altura}m"
 puts "Pesando #{peso}"
 puts "Seu IMC portanto é: #{imc}"
+puts "Sua categoria é: #{categorize(imc)}"
